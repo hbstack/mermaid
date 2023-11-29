44a201f8fa62df81c6bcbd87033d9478413df12c
razonyang <razonyang@gmail.com>
2023-11-29 13:34:12 +0800

    feat: re-render when switching themes

---
+++ assets/hb/modules/mermaid/init.ts
@@ -0,0 +1,34 @@
+(() => {
+  const elements = Array.from(document.querySelectorAll<HTMLElement>('pre.mermaid'))
+  if (!elements) {
+    return
+  }
+  elements.forEach((ele) => {
+    ele.setAttribute('data-src', ele.innerHTML);
+  })
+
+  const resetElements = (): Promise<void> => {
+    return new Promise((resolve) => {
+      elements.forEach((ele) => {
+        ele.innerHTML = ele.getAttribute('data-src')??'';
+        ele.removeAttribute('data-processed')
+      })
+      resolve()
+    })
+  }
+  const init = (theme: string): void => {
+    window.mermaid.initialize({ theme })
+    window.mermaid.init({ theme }, elements)
+  }
+
+  const theme = localStorage.getItem('hb-theme')
+  if (theme === 'dark') {
+    init('dark')
+  }
+
+  document.addEventListener('hb:theme', (e) => {
+    resetElements()
+      .then(init(e.detail.theme === 'dark' ? 'dark' : 'default'))
+      .catch(console.error)
+  })
+})()
